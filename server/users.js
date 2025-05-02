75bc5826bd9a69604f03f06b856fe174f85b6ba8
Timothy Ho <timothyhohyhtomit@gmail.com>
2025-05-02 13:41:26 -0400

    Wrote a User class and organise sample user data into class instances

--- server/users.js
+++ server/users.js
@@ -1,20 +1,26 @@
-export const users = {
-    1: {
-        name: "User1",
-        email: "user1@gmail.com",
-        password: "user1",
-        balance: 1000
-    },
-    2:{
-        name: "User2",
-        email: "user2@gmail.com",
-        password: "user2",
-        balance: 1000
-    },
-    3: {
-        name: "User3",
-        email: "user3@gmail.com",
-        password: "user3",
-        balance: 1000
+class User {
+    constructor(id, name, email, password, balance) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.balance = balance;
     }
+    // change balance
+    changeBalance(amount) {
+        // insufficient funds
+        if (this.balance + amount < 0) {
+            return false;
+        }
+        // change balance
+        this.balance += amount;
+        return true;
+    }
+}
+
+// hardcoded user data
+export const users = {
+    1: new User(1, "User1", "user1@gmail.com", "user1", 1000),
+    2: new User(2, "User2", "user2@gmail.com", "user2", 1000),
+    3: new User(3, "User3", "user3@gmail.com", "user3", 1000)
 };
