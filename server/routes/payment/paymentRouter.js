97817fe4a15c2cf17eb039f6e767dd196a065f3a
Timothy Ho <timothyhohyhtomit@gmail.com>
2025-05-04 20:08:53 -0400

    Changed /pay endpoint so that it now handles payment requests using database instead of hardcoded user data and that it calls a user-defined function that resides in the database.

--- server/routes/payment/paymentRouter.js
+++ server/routes/payment/paymentRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { users } from "../../users.js";
+import { sendQuery } from "../../db/conn.js";
 
 const paymentRouter = express.Router();
 
@@ -18,33 +18,22 @@ paymentRouter.post("/pay", async (req, res) => {
     if (isNaN(amountNum)) {
         return res.status(400).json({ error: "Invalid amount" });
     }
-    // check if payer and payee exist
-    if (!users[payerId]) {
-        return res.status(404).json({ error: "Payer not found" });
-    }
-    // find payee by email
-    const payeeId = Object.keys(users).find((userId) => users[userId].email === payeeEmail);
-    if (payeeId === undefined) {
-        return res.status(404).json({ error: "Payee not found" });
-    }
-    // check if payer and payee are the same
-    if (payerId === payeeId) {
-        return res.status(400).json({ error: "Payer and payee cannot be the same" });
-    }
     // payment amount must be positive
     if (amountNum <= 0) {
         return res.status(400).json({ error: "Payment amount must be positive" });
     }
     // try to initiate payment
-    const paymentSuccess = users[payerId].changeBalance(-amountNum);
+    const query = `
+        SELECT transfer(${payerId}, '${payeeEmail}', amount := ${amountNum}) AS new_balance;
+    `;
+    const result = await sendQuery(query);
     // check if payment was successful
-    if (!paymentSuccess) {
-        return res.status(400).json({ error: "Insufficient funds" });
+    if (result.length === 0 || !result[0]?.new_balance) {
+        // payment failed, send error message
+        return res.status(400).json({ error: "Payment failed" });
     }
-    // payment was successful, update payee balance
-    users[payeeId].changeBalance(amountNum);
-    // send success message
-    return res.status(200).json({ payerBalance: users[payerId].balance });
+    // payment was successful, send success message
+    return res.status(200).json({ new_balance: result[0].new_balance });
 });
 
 export default paymentRouter;
