a1dc9bf91160fdcb5f66fd5c9725f14224394e5d
Timothy Ho <timothyhohyhtomit@gmail.com>
2025-05-04 20:07:34 -0400

    Changed /signin endpoint so that it now handles signin requests using database instead of hardcoded user data

--- server/routes/auth/authRouter.js
+++ server/routes/auth/authRouter.js
@@ -1,8 +1,6 @@
 import express from "express";
 
-import { users } from "../../users.js";
-
-//import { sendQuery } from "../../db/conn.js";
+import { sendQuery } from "../../db/conn.js";
 
 const authRouter = express.Router();
 
@@ -14,33 +12,21 @@ authRouter.use(express.json());
 authRouter.post("/signin", async (req, res) => {
     // extract email and password
     const { email, password } = req.body;
-    /*
     // postgres query
     const query = `
-        SELECT * FROM users
+        SELECT id, name, balance
+        FROM users
         WHERE email = '${email}' AND password = '${password}'
     `;
     // get user data
     const userData = await sendQuery(query);
     // check if user exists
-    if (userData.length === 0) {
+    if (userData.length === 0 || userData[0].id === null) {
+        // user not found, send error message
         return res.status(401).json({ message: "Invalid credentials" });
     }
-    */
-    // currently, hardcoded user data imported from users.js
-    // find user
-    for (const userId of Object.keys(users)) {
-        if (users[userId].email == email && users[userId].password == password) {
-            // login success, send name and balance
-            return res.status(200).json({
-                id: userId,
-                name: users[userId].name,
-                balance: users[userId].balance
-            });
-        }
-    }
-    // login failed, send error message
-    return res.status(401).json({ message: "Invalid credentials" });
+    // return user id
+    return res.status(200).json(userData[0]);
 });
 
 export default authRouter;
